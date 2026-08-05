9bc0a71260db1884b6b3db5e2bf547cdc58f69fe
Justin53081 <justin53081@gmail.com>
2026-08-05 09:54:18 -0500

    Update app.py
    *

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ if st.button("Predict", type="primary"):
 
     st.metric("Churn Probability", f"{probability:.2%}")
     if risk == "High":
-        st.error(f"**Churn Risk: {risk}**\n\nTop Three Reasons for High Churn Risk: \n\n  • Device Type is Multi Device \n\n  • Low Tech Comfort Score \n\n  • Low Number of Products Owned", icon="🔴")
+        st.error(f"**Churn Risk: {risk}**\n\nTop Three Reasons for High Churn Risk: \n\n  * Device Type is Multi Device \n\n  * Low Tech Comfort Score \n\n  * Low Number of Products Owned", icon="🔴")
 
     elif risk == "Medium":
         st.warning(f"Churn Risk: {risk}", icon="🟡")
