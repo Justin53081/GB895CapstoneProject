d45ea56e7c747aaaf771967455d5cfdf2f02f9cc
Justin53081 <justin53081@gmail.com>
2026-08-05 11:57:30 -0500

    Update app.py
    plot try 1.1

--- app.py
+++ app.py
@@ -125,7 +125,7 @@ if st.button("Predict", type="primary"):
     st.metric("Churn Probability", f"{probability:.2%}")
     if risk == "High":
         st.error(f"**Churn Risk: {risk}**\n\nTop Three Reasons for High Churn Risk: \n\n  * Device Type is Multi Device \n\n  * Low Tech Comfort Score \n\n  * Low Number of Products Owned", icon="🔴")
-        st.error(f"{histograms["PRODUCTS_OWNED"]['fig']}")
+        st.pyplot(histograms["PRODUCTS_OWNED"]['fig'])
 
     elif risk == "Medium":
         st.warning(f"**Churn Risk: {risk}**\n\nTop Three Reasons for High Churn Risk: \n\n  * Device Type is Multi Device \n\n  * Low Tech Comfort Score \n\n  * Low Number of Products Owned", icon="🟡")
